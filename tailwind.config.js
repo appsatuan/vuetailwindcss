ca39e86ce173be188770dfb7bea840b749271228
arifsatuan <arifsatuan@gmail.com>
2024-02-19 14:52:49 +0700

    Add production build files in dist folder

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/**/*.html',
-    './src/**/*.vue',
-    // Add any other file types you want to include
-  ],
-  // Other Tailwind CSS configuration options...
-}
