d66567b7040f780a74271380d4a300b32873f81a
arifsatuan <arifsatuan@gmail.com>
2024-02-19 12:11:49 +0700

    first all

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    // Add any other file types you want to include
+  ],
+  // Other Tailwind CSS configuration options...
+}
