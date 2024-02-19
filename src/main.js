ee4b062cec10de737f4049bd290d9c67b8b5d967
unknown <arif.stwan@outlook.com>
2024-02-19 12:33:14 +0700

    axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import '@/assets/styles/index.css'
-import '@/assets/styles/app.css'
-import axios from 'axios'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "@/assets/styles/index.css";
+import axios from "axios";
+import "@/assets/styles/app.css";
 
-axios.defaults.baseURL = 'http://172.16.20.0:8082' 
+axios.defaults.baseURL = "http://172.16.20.0:8082";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
